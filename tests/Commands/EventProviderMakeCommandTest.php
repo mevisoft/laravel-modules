f9278fff212ec873f0792ad116b71b5af1554371
luisml <luismejiavevo@gmail.com>
2019-11-09 20:25:24 -0500

    add auth and event provider

---
+++ tests/Commands/EventProviderMakeCommandTest.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Nwidart\Modules\Tests\Commands;
+
+use Nwidart\Modules\Contracts\RepositoryInterface;
+use Nwidart\Modules\Tests\BaseTestCase;
+use Spatie\Snapshots\MatchesSnapshots;
+
+class EventProviderMakeCommandTest extends BaseTestCase
+{
+    use MatchesSnapshots;
+    /**
+     * @var \Illuminate\Filesystem\Filesystem
+     */
+    private $finder;
+    /**
+     * @var string
+     */
+    private $modulePath;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->modulePath = base_path('modules/Blog');
+        $this->finder = $this->app['files'];
+        $this->artisan('module:make', ['name' => ['Blog']]);
+    }
+
+    public function tearDown(): void
+    {
+        $this->app[RepositoryInterface::class]->delete('Blog');
+        parent::tearDown();
+    }
+
+    /** @test */
+    public function it_generates_a_new_service_provider_class()
+    {
+        $this->artisan('module:event-provider', ['module' => 'Blog']);
+
+        $this->assertTrue(is_file($this->modulePath . '/Providers/EventServiceProvider.php'));
+    }
+
+    /** @test */
+    public function it_generated_correct_file_with_content()
+    {
+        $this->artisan('module:event-provider', ['module' => 'Blog']);
+
+        $file = $this->finder->get($this->modulePath . '/Providers/EventServiceProvider.php');
+
+        $this->assertMatchesSnapshot($file);
+    }
+
+    /** @test */
+    public function it_can_change_the_default_namespace()
+    {
+        $this->app['config']->set('modules.paths.generator.provider.path', 'SuperProviders');
+
+        $this->artisan('module:event-provider', ['module' => 'Blog']);
+
+        $file = $this->finder->get($this->modulePath . '/SuperProviders/EventServiceProvider.php');
+
+        $this->assertMatchesSnapshot($file);
+    }
+
+    /** @test */
+    public function it_can_change_the_default_namespace_specific()
+    {
+        $this->app['config']->set('modules.paths.generator.provider.namespace', 'SuperProviders');
+
+        $this->artisan('module:event-provider', ['module' => 'Blog']);
+
+        $file = $this->finder->get($this->modulePath . '/Providers/EventServiceProvider.php');
+
+        $this->assertMatchesSnapshot($file);
+    }
+
+    /** @test */
+    public function it_can_overwrite_event_file_names()
+    {
+        $this->app['config']->set('modules.stubs.files.events/web', 'SuperEvents/web.php');
+        $this->app['config']->set('modules.stubs.files.events/api', 'SuperEvents/api.php');
+
+        $this->artisan('module:event-provider', ['module' => 'Blog', '--force' => true]);
+
+        $file = $this->finder->get($this->modulePath . '/Providers/EventServiceProvider.php');
+
+        $this->assertMatchesSnapshot($file);
+    }
+
+    /** @test */
+    public function it_can_overwrite_file(): void
+    {
+        $this->artisan('module:event-provider', ['module' => 'Blog']);
+        $this->app['config']->set('modules.stubs.files.events/web', 'SuperEvents/web.php');
+
+        $this->artisan('module:event-provider', ['module' => 'Blog', '--force' => true]);
+        $file = $this->finder->get($this->modulePath . '/Providers/EventServiceProvider.php');
+
+        $this->assertMatchesSnapshot($file);
+    }
+
+    /** @test */
+    public function it_can_change_the_custom_controller_namespace(): void
+    {
+        $this->app['config']->set('modules.paths.generator.controller.path', 'Base/Http/Controllers');
+        $this->app['config']->set('modules.paths.generator.provider.path', 'Base/Providers');
+
+        $this->artisan('module:event-provider', ['module' => 'Blog']);
+        $file = $this->finder->get($this->modulePath . '/Base/Providers/EventServiceProvider.php');
+
+        $this->assertMatchesSnapshot($file);
+    }
+}
